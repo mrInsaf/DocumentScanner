d13583e068fe4f512dc621495b8ad5513aa89846
mrInsaf <mr.insaf@list.ru>
2025-04-07 12:13:01 +0300

    Сделал более приятный ui в scanner модуле

---
+++ core/src/main/java/com/mrinsaf/core/ui/components/MainButton.kt
@@ -0,0 +1,48 @@
+package com.mrinsaf.core.ui.components
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun MainButton(
+    text: String,
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit,
+    enabled: Boolean = true,
+    contentPadding: PaddingValues = PaddingValues(vertical = 12.dp, horizontal = 24.dp),
+    colors: ButtonColors = ButtonDefaults.buttonColors(
+        containerColor = Color(0xFF0066CC),
+        contentColor = Color.White
+    )
+) {
+    Button(
+        onClick = onClick,
+        modifier = modifier
+            .fillMaxWidth()
+            .height(56.dp)
+            .then(modifier),
+        shape = RoundedCornerShape(8.dp),
+        colors = colors,
+        enabled = enabled,
+        contentPadding = contentPadding
+    ) {
+        Text(
+            text = text.uppercase(),
+            style = MaterialTheme.typography.bodyMedium
+        )
+    }
+}
---
+++ feature_scanner/src/main/java/com/mrinsaf/feature_scanner/ui/components/ScanStatusText.kt
@@ -0,0 +1,21 @@
+package com.mrinsaf.feature_scanner.ui.components
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import com.mrinsaf.core.data.model.DocumentDetails
+
+@Composable
+fun ScanStatusText(parsedQr: DocumentDetails?) {
+    val (text, color) = when(parsedQr) {
+        null -> "QR код документа пока не найден" to Color.Red
+        else -> "QR код документа найден!" to Color(0xFF2E7D32)
+    }
+
+    Text(
+        text = text,
+        style = MaterialTheme.typography.bodyMedium,
+        color = color
+    )
+}
